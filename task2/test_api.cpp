675e7ec9aa191a5f736c32d9ff7dc4dee1edf8fe
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-05-06 00:34:59 +0200

    perform basic testing on API level

--- task2/test_api.cpp
+++ task2/test_api.cpp
@@ -6,12 +6,41 @@ int main(){
     Result r, result;
     result = r.readJSON("topology.json");
 
-    for (int i=0; i< result.devices.size(); i++){
-        cout << "Device " << i << " ID: " << result.devices[i].get_id() << "\n";
-        cout << "Device " << i << " type: " << result.devices[i].get_type() << "\n";
+    cout << "readJSON test\n";
+    for (int i=0; i<result.topology_device_vector.size(); i++){
+        cout << "Topology " << result.topology_device_vector[i].first.get_id() << ":\n";
+        for (int j=0; j< result.topology_device_vector[i].second.size();j++){
+            cout << "Component id: " << result.topology_device_vector[i].second[j].get_id() << "\n";
+            cout << "Component type: " << result.topology_device_vector[i].second[j].get_type() << "\n";
+        }
     }
+    cout << "\n\n";
 
-    for (int i=0; i< result.topologies.size(); i++){
-        cout << "Topology " << i << " ID: " << result.topologies[i].get_id() << "\n";
+
+    cout << "queryTopologies test\n";
+    TopologyList t_l = result.queryTopologies();
+    for (auto i: t_l){
+        cout<< "Topology: " << i.get_id() << "\n";
     }
+    cout << "\n\n";
+
+
+    cout << "queryDevices test\n";
+    DeviceList dv_l = result.queryDevices("top1");
+    for (auto i: dv_l){
+        cout<< "Device in top1: " << i.get_id() << "-->" << i.get_type() << "\n";
+    }
+    cout << "\n\n";
+
+
+    cout << "deleteTopology test\n";
+    Result new_result = result.deleteTopology("top1");
+    if (new_result.topology_device_vector.size() == 0){
+        cout << "Empty vector\n";
+    }else{
+        cout << "Nothing deleted\n";
+    }
+    cout << "\n\n";
+
+    
 }
