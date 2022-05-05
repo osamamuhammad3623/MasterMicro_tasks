9c5c3938cd185a7a2ead307d8eb9429d95c7f536
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-05-05 21:35:38 +0200

    finish Topology class implementation

---
+++ task2/topology.cpp
@@ -0,0 +1,17 @@
+#include "topology.h"
+
+void Topology::set_id(string id){
+    this->topology_id = id;
+}
+
+string Topology::get_id(){
+    return this->topology_id;
+}
+
+Topology::Topology(){
+
+}
+
+Topology::~Topology(){
+
+}
