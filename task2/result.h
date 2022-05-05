b3a22ac6e2003684100ce1867ccf7471be871e11
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-05-06 00:34:23 +0200

    Implement readJSON, queryTopologies, deleteTopology and queryDevices

--- task2/result.h
+++ task2/result.h
@@ -9,16 +9,36 @@ typedef vector<Topology> TopologyList;
 
 class Result{
 public:
-    /* attributes */
-    TopologyList topologies;
-    DeviceList devices;
+    vector<pair<Topology, DeviceList>> topology_device_vector;
 
     /* Methods */
+
+    /*
+    Description: A function that reads a JSON file and returns topologies and devices in that file
+            - Input: file name (relative/absolute path)
+            - Output: Result object containing topologies and devices in the JSON file
+    */
     Result readJSON(string file_name);
-    Result writeJSON();
+
+    Result writeJSON(string TopologyID);
+
+    /*
+    Description: A function that iterates through vector of pairs of connected devices (topologies),
+                    and returns TopologyList of all topologies found
+    */
     TopologyList queryTopologies();
+
+    /*
+    Description: A function to delete a specific topology and
+                    return updated topology_device_vector in a new Result object
+    */
     Result deleteTopology(string topology_id);
+
+    /*
+    Description: A function that returns all devices connected together in a specific topoligy
+    */
     DeviceList queryDevices(string topology_id);
+
     DeviceList queryDevicesWithNetlistNode(string TopologyID, string NetlistNodeID);
 
     Result();
